e08ed50bfd8bc317b26207d4eeadb7587cd4703f
Yuvraj Rathva <rathva.1@iitj.ac.in>
2023-08-03 09:11:55 +0530

    convert to vite project

--- index.js
+++ index.js
@@ -1,40 +1,29 @@
-import {process} from "./env.js";
+import { process } from '/env'
+import { Configuration, OpenAIApi } from 'openai'
 
-const apiKey = process.env.OPENAI_API_KEY;
-const url = "https://api.openai.com/v1/completions";
+const setupTextarea = document.getElementById('setup-textarea')
+const setupInputContainer = document.getElementById('setup-input-container')
+const movieBossText = document.getElementById('movie-boss-text')
 
-const setupTextarea = document.getElementById("setup-textarea");
-const setupInputContainer = document.getElementById("setup-input-container");
-const movieBossText = document.getElementById("movie-boss-text");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(configuration)
 
 document.getElementById("send-btn").addEventListener("click", () => {
   // if (setupTextarea.value) {
-  setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`;
-  movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`;
+  setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
+  movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
   // }
-  fetchBotReply();
-});
+  fetchBotReply()
+})
 
-function fetchBotReply() {
-  /*
-Challenge:
-  1. Make a fetch request to OpenAi API. 
-  2. The prompt should request an enthusiastic response
-     in no more than 5 words. 
-  3. For now you can just log out the completion to check 
-     it's working. 
-*/
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${apiKey}`,
-    },
-    body: JSON.stringify({
-      model: "text-davinci-003",
-      prompt: "Sound enthusiastic in five words or less.",
-    }),
+async function fetchBotReply() {
+  const response = await openai.createCompletion({
+    'model': 'text-davinci-003',
+    'prompt': 'Sound enthusiastic in five words or less.'
   })
-    .then((response) => response.json())
-    .then((data) => (movieBossText.innerText = data.choices[0].text));
+  console.log(response)
+  movieBossText.innerText = response.data.choices[0].text.trim()
 }
